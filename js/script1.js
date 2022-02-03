05c7b18fc9f0bc0e4009cc90c0176a6e264ae098
AkashMarkad <akashmarkad2210@gmail.com>
2022-02-03 22:54:55 +0530

    'uploaded'

---
+++ js/script1.js
@@ -0,0 +1,27 @@
+const name = document.getElementById('Name');
+const email = document.getElementById('email');
+const msg = document.getElementById('addmsg');
+const form = document.getElementById('form');
+const btn = document.getElementById('btnS');
+
+console.log("hello");
+
+const db = firebase.firestore();
+
+btn.addEventListener('click',(e)=>{
+    console.log("Hi");
+});
+
+form.addEventListener('submit', (e)=>{
+    console.log("clicked");
+    e.preventDefault();
+
+    db.collection('user').add({
+        Name: name.value,
+        Email: email.value,
+        Msg: msg.value
+    })
+    .then(()=>{
+        console.log("Submitted");
+    })
+});
